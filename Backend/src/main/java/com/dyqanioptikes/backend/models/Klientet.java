06eb8b3829e0b3628d7328ce0daf2627471e13ba
BleartAl <ba73664@ubt-uni.net>
2026-04-04 21:55:51 +0200

    Lidhja me MSSQL dhe krijimi i entitetit Klientet

---
+++ Backend/src/main/java/com/dyqanioptikes/backend/models/Klientet.java
@@ -0,0 +1,21 @@
+package com.dyqanioptikes.backend.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "Klientet")
+@Data
+public class Klientet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String emri;
+    private String mbiemri;
+    private String email;
+    private String telefoni;
+    private LocalDate dataLindjes;
+    private String adresa;
+}
