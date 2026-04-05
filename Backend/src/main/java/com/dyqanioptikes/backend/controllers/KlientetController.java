68973385e2f0a4a6b005d1bb9207d9c3cd26ec5c
BleartAl <ba73664@ubt-uni.net>
2026-04-05 21:54:16 +0200

    shtimi i KlientetController per REST API

---
+++ Backend/src/main/java/com/dyqanioptikes/backend/controllers/KlientetController.java
@@ -0,0 +1,29 @@
+package com.dyqanioptikes.backend.controllers;
+
+import com.dyqanioptikes.backend.models.Klientet;
+import com.dyqanioptikes.backend.repositories.KlientetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/klientet")
+@CrossOrigin(origins = "http://localhost:3000") // Lejon React-in të marrë të dhënat
+public class KlientetController {
+
+    @Autowired
+    private KlientetRepository klientetRepository;
+
+    // Merr të gjithë klientët nga databaza
+    @GetMapping
+    public List<Klientet> getAllKlientet() {
+        return klientetRepository.findAll();
+    }
+
+    // Shto një klient të ri
+    @PostMapping
+    public Klientet createKlient(@RequestBody Klientet klient) {
+        return klientetRepository.save(klient);
+    }
+}
