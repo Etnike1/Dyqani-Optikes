6ddb6a9ba371cec1a47b6b9a32bc90c5d1a5c228
VleraZyhrani <vz73277@ubt-uni.net>
2026-05-23 15:42:45 +0200

    Added JWT authentication

--- Backend/src/main/java/com/dyqanioptikes/backend/security/JwtFilter.java
+++ Backend/src/main/java/com/dyqanioptikes/backend/security/JwtFilter.java
@@ -1,4 +1,73 @@
 package com.dyqanioptikes.backend.security;
 
-public class JwtFilter {
+import com.dyqanioptikes.backend.services.UserDetailsServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtils jwtUtils;
+    private final UserDetailsServiceImpl userDetailsService;
+
+    public JwtFilter(JwtUtils jwtUtils,
+                     UserDetailsServiceImpl userDetailsService) {
+        this.jwtUtils = jwtUtils;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String headerAuth = request.getHeader("Authorization");
+
+        String token = null;
+        String username = null;
+
+        if (headerAuth != null && headerAuth.startsWith("Bearer ")) {
+            token = headerAuth.substring(7);
+            username = jwtUtils.getUsernameFromJwtToken(token);
+        }
+
+        if (username != null &&
+                SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails =
+                    userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtils.validateJwtToken(token)) {
+
+                UsernamePasswordAuthenticationToken authentication =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource()
+                                .buildDetails(request)
+                );
+
+                SecurityContextHolder.getContext()
+                        .setAuthentication(authentication);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
--- Backend/src/main/java/com/dyqanioptikes/backend/services/UserDetailsServiceImpl.java
+++ Backend/src/main/java/com/dyqanioptikes/backend/services/UserDetailsServiceImpl.java
@@ -1,4 +1,38 @@
 package com.dyqanioptikes.backend.services;
 
-public class UserDetailsServiceImpl {
+import com.dyqanioptikes.backend.models.User;
+import com.dyqanioptikes.backend.repositories.UserRepository;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.*;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User nuk u gjet"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                Collections.singletonList(
+                        new SimpleGrantedAuthority("ROLE_USER")
+                )
+        );
+    }
 }
